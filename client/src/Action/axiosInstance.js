808f8eb97e7c1c5b7e18f4267a7ab5e0683bed4a
Y.kazama <Y.Kazama1996@gmail.com>
2024-02-14 20:47:06 +0800

    update axios api from localhost to ec2 public ip

--- client/src/Action/axiosInstance.js
+++ client/src/Action/axiosInstance.js
@@ -2,8 +2,11 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
+let ec2_publicIp = "http://3.27.70.254:8080";
+let local_machineIp = "http://localhost:8080";
+
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: ec2_publicIp,
 });
 
 export default instance;
